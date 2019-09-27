0c335d66469301e227b3e3bed97626c2b58bb44b
Troy Rios <troy.rios@gmail.com>
2019-09-27 13:36:41 +0000

    Random number guess algorgorithm

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+var upper = 10000;
+var intRand = getRandomNumber(upper);
+var guess;
+var attempts = 0;
 function getRandomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
+
+while (guess !== intRand)
+{
+  guess = getRandomNumber(upper);
+  attempts += 1;
+}
+
+document.write("<p> The random number was: " + intRand + "<p>");
+document.write("<p>It took the computer " + attempts + "attempts to guess the number.<p>");
